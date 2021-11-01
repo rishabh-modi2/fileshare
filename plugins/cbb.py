b70133dd57d127c22e6c20c72629b16092490cfd
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2021-11-01 19:09:59 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ demo : <code>https://t.me/Usidknxnxjxj_bot?start=Z2V0LTExMTI4MzY0Nzg3OTI2NDMtMTE0ODg5NTk4NjY1NjMxMQ==</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/siriyourdad'>Click here</a>\n○ Channel : @pornuserbotlink\n○ Support Group : @siriyourdad</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
